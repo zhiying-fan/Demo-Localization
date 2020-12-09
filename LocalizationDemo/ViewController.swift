7d84c06e706e0b6622f1649d6bce216679558236
Zhiying Fan <zyfan@thoughtworks.com>
2020-12-09 10:48:07 +0800

    Build basic UI

--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var titleLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    titleLabel.text = NSLocalizedString("Kitty is so cute", comment: "Title")
   }
-
-
 }
-
